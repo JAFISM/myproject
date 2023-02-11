8f9eb1ffaf2dc0ad1727bcb43551b276aa4ff653
Jafis <jafism2000@gmail.com>
2023-02-11 19:24:43 +0530

    commit

--- lib/Login_Page.dart
+++ lib/Login_Page.dart
@@ -16,11 +16,12 @@ class HomePage extends StatelessWidget {
         width: double.infinity,
         decoration: BoxDecoration(
             gradient: LinearGradient(
+              //end: Alignment.topRight,
                 begin: Alignment.topCenter,
                 colors: [
-                  green,
+                  //green,
                   teal,
-                  Colors.white
+                  teal,
                 ]
             )
         ),
@@ -58,11 +59,12 @@ class HomePage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(10),
                               boxShadow: [BoxShadow(
                                   color: teal,
-                                  blurRadius: 10,
-                                  offset: Offset(1, 1)
+                                  blurRadius: 5,
+                                  offset: Offset(0, 0.5)
                               )]
                           ),
                           child: Column(
+                            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
                               Container(
                                 padding: EdgeInsets.all(10),
@@ -94,51 +96,59 @@ class HomePage extends StatelessWidget {
                           ),
                         ),
                         SizedBox(height: 40,),
-                        Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
+                        TextButton(onPressed: (){}, child: Text("Forgot password",style: TextStyle(color: black10),)),
+                        //Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
                         SizedBox(height: 40,),
-                        Container(
+                        SizedBox(
                           height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: teal
-                          ),
-                          child: Center(
-                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          width: MediaQuery.of(context).size.width/2,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: black10),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: teal,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
                           ),
                         ),
                         SizedBox(height: 50,),
-                        Text("Continue with social media", style: TextStyle(color: Colors.grey),),
-                        SizedBox(height: 30,),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: Colors.blue
-                                ),
-                                child: Center(
-                                  child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: 30,),
-                            Expanded(
-                              child: Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: Colors.red
-                                ),
-                                child: Center(
-                                  child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                                ),
-                              ),
-                            )
-                          ],
-                        )
+                        TextButton(onPressed: (){}, child: Text("Don't have an account? Sign UP",style: TextStyle(color: black10),))
+                        // Text("Don't have an account", style: TextStyle(color: Colors.grey),),
+                        //SizedBox(height: 30,),
+                        // Row(
+                        //   children: <Widget>[
+                        //     Expanded(
+                        //       child: Container(
+                        //         height: 50,
+                        //         decoration: BoxDecoration(
+                        //             borderRadius: BorderRadius.circular(50),
+                        //             color: Colors.blue
+                        //         ),
+                        //         child: Center(
+                        //           child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        //         ),
+                        //       ),
+                        //     ),
+                        //     SizedBox(width: 30,),
+                        //     Expanded(
+                        //       child: Container(
+                        //         height: 50,
+                        //         decoration: BoxDecoration(
+                        //             borderRadius: BorderRadius.circular(50),
+                        //             color: Colors.red
+                        //         ),
+                        //         child: Center(
+                        //           child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        //         ),
+                        //       ),
+                        //     )
+                        //   ],
+                        // ),
                       ],
                     ),
                   ),
